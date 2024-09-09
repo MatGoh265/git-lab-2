d493b3c2e25093f518d8732446d3df789d85d959
MatGoh265 <mg639224@ohio.edu>
2024-09-09 15:37:23 -0400

    text

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mathew Goh
+ *        Date: 9/9
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
